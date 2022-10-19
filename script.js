d8d53814f62ea021f4b34bfb3f05fdc8bed89784
Danielk953 <115681045+Danielk953@users.noreply.github.com>
2022-10-19 15:41:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ownerName = "Daniel Kovacs"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let userName = "Danielk953"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;

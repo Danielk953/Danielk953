3bdba3ded7a9c938a31a60d97370a15974fc6679
Daniel Kovacs <danielk9538@hstat.org>
2023-07-05 03:25:24 +0000

    template, created modals, personalized

--- script.js
+++
@@ -1,9 +0,0 @@
-let ownerName = "Daniel Kovacs"; // update the part BETWEEN the "quotes"
-let userName = "danielk9538"; // same here
-
-document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
-});
-
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
